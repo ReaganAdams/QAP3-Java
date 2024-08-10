cb64f079d153e4f04e4685957175568324796483
ReaganAdams <152213402+ReaganAdams@users.noreply.github.com>
2024-08-10 01:31:04 -0230

    Student.java

---
+++ Student.java
@@ -0,0 +1,33 @@
+class Student extends Person {
+    protected String myIdNum;    // Student Id Number
+    protected double myGPA;      // grade point average
+
+    public Student(String name, int age, String gender, String idNum, double gpa) {
+        super(name, age, gender);
+        myIdNum = idNum;
+        myGPA = gpa;
+    }
+
+    // Getter methods
+    public String getIdNum() {
+        return myIdNum;
+    }
+
+    public double getGPA() {
+        return myGPA;
+    }
+
+    // Setter methods
+    public void setIdNum(String idNum) {
+        myIdNum = idNum;
+    }
+
+    public void setGPA(double gpa) {
+        myGPA = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", ID: " + myIdNum + ", GPA: " + myGPA;
+    }
+}
